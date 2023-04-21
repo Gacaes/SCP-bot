85c7fa5c585d2021b7c724fc275a8f6601c4b6a4
Gacaes <77614712+Gacaes@users.noreply.github.com>
2023-04-21 23:43:22 +0000

    Added gitignore

--- modules/utils.py
+++ modules/utils.py
@@ -1,6 +1,20 @@
 def kill(msg=""):
     raise Exception(f'Program killed{": "+str(msg) if msg!="" else ""}')
 
+def msg_short(msg):
+    sent=''
+    array=[]
+    while len(msg):
+        index=msg.find('.')
+        if len(sent)+index+2<2000:
+            sent+=msg[:index+1]
+            msg=msg[index+1:]
+        else:
+            array.append(str(sent))
+            sent=''
+    array.append(str(sent))
+    return array
+
 class on_ready_msg():
     def __init__(self,msg='<cog> Cog Loaded'):
         """Set a custom message to print to the terminal when the cog is imported
@@ -12,5 +26,11 @@ class on_ready_msg():
 
 class command():
     def __init__(self,msg,aliases=[]):
+        """Set a custom message to print to the terminal when the cog is imported
+
+        Args:
+            msg (string): The basic message to be print to discord when the command is run
+            aliases (list (string)): A list of string aliases to use for the command
+        """
         self.aliases=aliases
-        self.msg=msg
+        self.msg=msg_short(msg)
